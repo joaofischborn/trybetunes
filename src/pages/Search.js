12d3665e625454a06d4aa0370b9b11ab38f213ac
João Vitor <joaovitorfischborn@icloud.com>
2022-07-05 15:29:37 -0300

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import LoadingMessage from './LoadingMessage';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       button: true,
+      input: '',
+      inputValue: '',
+      loading: false,
+      response: false,
+      albumsList: [],
+      notFound: false,
     };
   }
 
   handleChange = (e) => {
+    this.setState({ input: e.target.value });
     const minCaracters = 1;
     if (e.target.value.length > minCaracters) {
       this.setState({ button: false });
@@ -18,23 +28,68 @@ class Search extends React.Component {
     }
   };
 
+  searchAlbum = async () => {
+    const { input } = this.state;
+    this.setState({ loading: true });
+    const albums = await searchAlbumsAPI(input);
+    this.setState({ albumsList: albums,
+      inputValue: input,
+      input: '',
+      loading: false,
+      response: true });
+    if (albums.length === 0) {
+      this.setState({ notFound: true, response: false });
+    } else {
+      this.setState({ notFound: false });
+    }
+  }
+
   render() {
-    const { button } = this.state;
+    const { button,
+      input,
+      loading, response, inputValue, albumsList, notFound } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <input
-          data-testid="search-artist-input"
-          type="text"
-          onChange={ this.handleChange }
-        />
-        <button
-          data-testid="search-artist-button"
-          type="button"
-          disabled={ button }
-        >
-          Pesquisar
-        </button>
+        { loading ? <LoadingMessage /> : (
+          <>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.handleChange }
+              value={ input }
+            />
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              disabled={ button }
+              onClick={ this.searchAlbum }
+            >
+              Pesquisar
+            </button>
+            { response
+            && (
+              <span>
+                {`Resultado de álbuns de:
+                ${inputValue}`}
+              </span>
+            )}
+          </>
+        )}
+        { notFound ? <span>Nenhum álbum foi encontrado</span>
+          : albumsList.map((album) => (
+            <div key={ album.collectionId }>
+              <Link
+                data-testid={ `link-to-album-${album.collectionId}` }
+                to={ `/album/${album.collectionId}` }
+              >
+                Álbum
+              </Link>
+              <p>{album.artistName}</p>
+              <p>{album.collectionName}</p>
+              <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+            </div>
+          ))}
       </div>
     );
   }
