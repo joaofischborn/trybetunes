361c2b8125465a9060bbad8e60c0d526e06b8e09
João Vitor <joaovitorfischborn@icloud.com>
2022-07-05 19:49:22 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
           <Switch>
             <Route path="/" exact component={ Login } />
             <Route path="/search" component={ Search } />
-            <Route path="/album" component={ Album } />
+            <Route path="/album/:id" component={ Album } />
             <Route path="/favorites" component={ Favorites } />
             <Route path="/profile/edit" component={ ProfileEdit } />
             <Route path="/profile" component={ Profile } />
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,13 +1,54 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      songs: [],
+      albumInfo: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getMusic();
+  }
+
+  getMusic = async () => {
+    const { match: { params: { id } } } = this.props;
+    const musics = await getMusics(id);
+    this.setState({ songs: [...musics], albumInfo: musics[0] });
+  }
+
   render() {
+    const { songs, albumInfo } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <p data-testid="artist-name">{albumInfo.artistName}</p>
+        <p data-testid="album-name">{albumInfo.collectionName}</p>
+        { songs.filter((song) => song.trackName).map((song) => (
+          <MusicCard
+            key={ song.trackNumber }
+            trackName={ song.trackName }
+            previewUrl={ song.previewUrl }
+          />
+        ))}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: propTypes.shape({
+    params: propTypes.shape({
+      id: propTypes.string,
+    }),
+  }),
+}.isRequired;
+
 export default Album;
